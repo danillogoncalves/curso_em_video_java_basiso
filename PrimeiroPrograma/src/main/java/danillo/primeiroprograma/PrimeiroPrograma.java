c91a4fdc0eda8e30a577fed3820f3a8f3ac3986d
Danillo <danillogoncalves001@gmail.com>
2022-10-29 14:23:20 -0300

    :heavy_mark_check: feat: Aula 3 e exercícios da aula 3.

---
+++ PrimeiroPrograma/src/main/java/danillo/primeiroprograma/PrimeiroPrograma.java
@@ -0,0 +1,10 @@
+// Nome do pacote
+package danillo.primeiroprograma;
+
+// Nome da classe
+public class PrimeiroPrograma {
+    // Nome do Método
+    public static void main(String[] args) {
+        System.out.print("Hello World!");
+    }
+}
