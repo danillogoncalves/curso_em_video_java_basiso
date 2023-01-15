35251e225e37c9b95592bb4483000c979c485000
Danillo <danillogoncalves001@gmail.com>
2023-01-15 18:37:03 -0300

    Java Básico [40 Horas]  Java  Aula 7 – Operadores Aritméticos e Classe Math

---
+++ OperadoresAritimeticos/src/operadoresaritimeticos/OperadoresAritimeticos.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package operadoresaritimeticos;
+
+/**
+ *
+ * @author danillo-nitro-5
+ */
+public class OperadoresAritimeticos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        /* int n1  = 3;
+        int n2 = 5;
+        float m = (n1 + n2) / 2;
+        System.out.println("A média é iguaç a " + m + ".");*/
+        
+//        int numero = 5;
+//        numero++;
+//        System.out.println(numero);
+//        numero--;
+//        System.out.println(numero);
+//        int valor = 5 + numero++;
+//        System.out.println(valor);
+//        int valor = 5 + numero--;
+//        System.out.println(valor);
+        /*int valor1 = 5 + ++numero;
+        System.out.println(valor1);
+        System.out.println(numero);
+        int valor2 = 5 + --numero;
+        System.out.println(valor2);
+        System.out.println(numero);*/
+//        int x = 4;
+//        x += 2;
+//        System.out.println(x);
+//        int x = 4;
+//        x *= 2;
+//        System.out.println(x);
+//        int x = 4;
+//        x -= 2;
+//        System.out.println(x);
+//        int x = 4;
+//        x /= 2;
+//        System.out.println(x);
+        System.out.println(Math.PI);
+        System.out.println(Math.pow(5, 2));
+        System.out.println(Math.sqrt(25));
+        System.out.println(Math.cbrt(27));
+        System.out.println(Math.abs(-10));
+        System.out.println(Math.floor(3.9));
+        System.out.println(Math.ceil(4.2));
+        System.out.println(Math.round(5.6));
+        
+        float v = 8.4f;
+        int ar = (int) Math.floor(v);
+        System.out.println(ar);
+        
+        System.out.println((int)(1 + Math.random() * 10));
+
+    }
+    
+}
