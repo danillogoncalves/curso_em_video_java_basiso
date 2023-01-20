87b16a832a35d48dffbb8c39135efc2613f65442
Danillo <danillogoncalves001@gmail.com>
2023-01-19 22:08:12 -0300

    Java Básico [40 Horas]  Java  Aula 10 – Estruturas Condicionais (Parte 2)

---
+++ ProgramaPernas/src/programapernas/ProgramaPernas.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package programapernas;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author danillo-nitro-5
+ */
+public class ProgramaPernas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("Quantidade de pernas: ");
+        int pernas = teclado.nextInt();
+        System.out.print("Isso é um ");
+        String tipo;
+        switch (pernas) {
+            case 1:
+                tipo = "Saci";
+                break;
+            case 2:
+                tipo = "Bípede";
+                break;
+            case 4:
+                tipo = "Quadrúpede";
+                break;
+            case 6:
+                tipo = "Aranha";
+                break;
+            default:
+                tipo = "E.T.";
+                break;
+        }
+        System.out.println(tipo);
+    }
+    
+}
